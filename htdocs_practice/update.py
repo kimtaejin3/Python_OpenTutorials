feb4ce7a180470b3ed83dc73e357fe31757566e5
Taejin Kim <60560836+kimtaejin3@users.noreply.github.com>
2020-02-04 17:35:41 +0900

    Add files via upload

---
+++ htdocs_practice/update.py
@@ -0,0 +1,41 @@
+#!C:\Program Files\Python38\python.exe
+print("Content-Type: text/html")
+print()
+import cgi, os
+
+files=os.listdir('data')
+listStr=''
+for item in files:
+    listStr = listStr + '<li><a href="index.py?id={name}">{name}</a></li>'.format(name=item)
+form=cgi.FieldStorage()
+
+if 'id' in form:
+    pageId=form["id"].value
+    f=open('data/'+ pageId,'r')
+    description=f.read()
+else:
+    pageId='Welcome'
+    description='Hello Web'
+
+
+print('''<!doctype html>
+<html>
+<head>
+  <title>WEB1 - Welcome</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="index.py">WEB</a></h1>
+  <ol>
+    {listStr}
+  </ol>
+  <a href="create.py">create</a>
+ <form action="process_update.py" method="post">
+      <p><input type="hidden" name="pageId" value="{form_default_title}"></p>
+      <p><input type="text" name="title" placeholder="title" value="{form_default_title}"></p>
+      <p><textarea rows="4" name="description" placeholder="description">{form_default_description}</textarea></p>
+      <p><input type="submit"></p>
+  </form>
+</body>
+</html>
+'''.format(title=pageId,des=description,listStr=listStr,form_default_title=pageId,form_default_description=description))
